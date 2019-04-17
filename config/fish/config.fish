dfc2cecfbae88a423b325d913294bedf8eb54363
geoTrader <geometrytrader@gmail.com>
2019-04-18 07:30:40 +0900

    start to use alias!

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,41 @@
 export MANPAGER="nvim -c 'set ft=man' -"
 set PATH ~/.local/bin $PATH
+set QT_AUTO_SCREEN_SCALE_FACTOR 1
+set _JAVA_AWT_WM_NONREPARENTING 1
+set WINIT_HIDPI_FACTOR 1.0
 
 thefuck --alias | source
+
+# alias
+alias a='atool'
+alias b='baobab'
+alias c='code'
+alias d='discord'
+alias e='enpas'
+alias ex='exit'
+alias f='fuck'
+alias g='git'
+alias gc='git commit'
+alias gl='glances'
+alias gr='grep --color=auto'
+alias h='history'
+#alias i=''
+alias j='jmeter'
+alias k='kill'
+alias l='ls -a'
+alias m='mkdir'
+alias n='nvim'
+#alias o=''
+alias p='pycharm'
+alias q='qutebrowser'
+alias r='rofi-show run'
+alias s='sudo'
+alias t='tig'
+alias u='sudo umount'
+alias v='nvim'
+alias vm='vifm .'
+alias w='wget'
+alias x='xmonad --recompile'
+alias y='yay'
+alias ya='yarn'
+alias z='zathura'
